e9de36d10594f58e860939da2da5811e1137172f
harmeet-singh-project <its.harmeet.singh@outlook.com>
2025-10-09 14:53:06 -0400

    Removing Demo Files

--- backend/auth-users-service/src/main/java/com/foodrescue/auth/repository/StudentReactiveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodrescue.auth.repository;
-
-import com.foodrescue.auth.entity.Student;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
-
-public interface StudentReactiveRepository extends ReactiveMongoRepository<Student, String> {
-    Flux<Student> findByAge(int age);
-}
