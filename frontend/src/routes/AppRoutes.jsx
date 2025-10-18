271b9c59e38c4cc33e49fe825ac275cce36cd047
andresl123 <andreslopes2009@gmail.com>
2025-10-18 19:02:45 -0400

    merge from MTC-41-login/signup/address

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "../pages/signup/SignupPage";
-import RequestCodePage from "../pages/resetpassword/RequestCodePage";
-import ResetPasswordPage from "../pages/resetpassword/ResetPasswordPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/authentication" element={<SignupPage />} />
-        <Route path="/resetpassword" element={<RequestCodePage />} />
-        <Route path="/createnewpassword" element={<ResetPasswordPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default AppRoutes;
