2c363794effed9fb658e786157e723f88714a6f8
abhishek22xi <abhishek22xi@gmail.com>
2025-11-12 00:39:54 -0500

    removed courier-verification

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignupPage from "../pages/signup/SignupPage";
 import UserDashboard from "../pages/dashboards/userDashboard/UserDashboard";
-import CourierVerificationPage from "../pages/CourierVerificationPage";
 import ProfilePage from "../pages/common/ProfilePage";
 import RescueOrdersPage from "../pages/receiver/RescueOrdersPage";
 import ordersData from "../mock/orders.json";
@@ -14,7 +13,6 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Navigate to="/authentication" replace />} />
         <Route path="/authentication" element={<SignupPage />} />
-        <Route path="/courier-verification" element={<CourierVerificationPage />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/orders" element={<RescueOrdersPage data={ordersData} />} />
