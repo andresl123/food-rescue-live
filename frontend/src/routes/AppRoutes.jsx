af681db2c2172f0d7a71c73c42f07bf0f404358a
harmeet-singh-project <its.harmeet.singh@outlook.com>
2025-11-12 00:32:28 -0500

    Removed mock data

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,7 +6,6 @@ import UserDashboard from "../pages/dashboards/userDashboard/UserDashboard";
 import CourierVerificationPage from "../pages/CourierVerificationPage";
 import ProfilePage from "../pages/common/ProfilePage";
 import RescueOrdersPage from "../pages/receiver/RescueOrdersPage";
-import ordersData from "../mock/orders.json";
 import FoodItemList from "../components/dashboards/donor/FoodItemList";
 import TestLotImport from "../pages/common/TestLotImport";
 
@@ -29,7 +28,7 @@ function AppRoutes() {
         <Route path="/courier-verification" element={<CourierVerificationPage />} />
         <Route path="/dashboard" element={<UserDashboard />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/orders" element={<RescueOrdersPage data={ordersData} />} />
+        <Route path="/orders" element={<RescueOrdersPage />} />
         <Route path="/bulk-import" element={<TestLotImport />} />
         <Route path="/food-items" element={<FoodItemList />} />
 
